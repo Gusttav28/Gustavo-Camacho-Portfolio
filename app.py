7827d4dc15cfd4020a4a29a6a51a3a5f9bdb95ca
Gustavo Camacho <Gusttav28@GMAIL.COM>
2025-01-31 17:59:44 -0600

    sdf

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
+@app.route("/")
+def index():
+    return render_template("index.html")
 
+@app.route("/aboutme")
+def aboutme():
+    return render_template("aboutme.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+if __name__ == "__main__":
+    app.run(port= 3002, debug=True)
