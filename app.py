8c401abfe47375551bdce8af83a67372b9f5c3fd
Gustavo Camacho <Gusttav28@GMAIL.COM>
2025-01-31 15:02:39 -0600

    afd

--- app.py
+++ app.py
@@ -1,26 +1,6 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-@app.route("/aboutme")
-def aboutme():
-    return render_template("aboutme.html")
-
-@app.route("/projects")
-def projects():
-    return render_template("projects.html")
-
-@app.route("/test")
-def test():
-    return "test"
-
-@app.route("/test2")
-def test2():
-    return "test2"
+from Controller import constructure
 
 if __name__ == "__main__":
-    app.run(port=3023, debug=True)
+    app = constructure.app(__name__)
+    app.app_routes()
+    app.app_main()
