cabc89ad2ae6e167f0442e44801b88b0e61cc52b
Gusttav28 <gusttav28@gmail.com>
2025-02-27 08:12:55 +0000

    DSF

--- app.py
+++ app.py
@@ -1,26 +1,11 @@
-from flask import Flask, render_template
+
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+@app.route('/')
+def hello_world():
+    return 'Hello from Flask!'
 
-@app.route("/aboutme")
-def aboutme():
-    return render_template("aboutme.html")
-
-@app.route("/projects")
-def projects():
-    return render_template("projects.html")
-
-@app.route("/home")
-def home():
-    return render_template("home.html")
-
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port= 3025, debug=True)
